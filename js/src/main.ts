13f84fb5a856a54bd3adcca38199d982975f218a
Manuel Odendahl <wesen@ruinwesen.com>
2026-01-10 20:56:37 -0500

    Demo: show SVG processing output

--- js/src/main.ts
+++ js/src/main.ts
@@ -1,14 +1,36 @@
 import dayjs from "dayjs";
-import { sumBy } from "lodash";
+import * as lodash from "lodash";
 import logo from "./assets/logo.svg";
 import * as exec from "exec";
 import * as fs from "fs";
 
 export function run(): string {
   var items: Array<{ n: number }> = [{ n: 2 }, { n: 3 }];
-  var total = sumBy(items, "n");
+  var total = lodash.sumBy(items, "n");
   var stamp = dayjs().format("YYYY-MM-DD");
-  var payload = stamp + ":" + total + ":" + logo.length;
+  var logoChars = logo.split("");
+  var counts = lodash.countBy(logoChars, function (ch: string) {
+    return ch;
+  });
+  var tagCount = counts["<"] || 0;
+  var checksum = lodash.reduce(
+    logoChars,
+    function (acc: number, ch: string) {
+      return (acc + ch.charCodeAt(0)) % 100000;
+    },
+    0
+  );
+  var payload =
+    "date=" +
+    stamp +
+    " sum=" +
+    total +
+    " svgLen=" +
+    logo.length +
+    " svgTags=" +
+    tagCount +
+    " svgCsum=" +
+    checksum;
 
   fs.writeFileSync("/tmp/goja-bun.txt", payload);
 
